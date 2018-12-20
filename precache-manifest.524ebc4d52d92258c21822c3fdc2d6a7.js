14d1d12aaefdbc805a2ee580290c51587fa2ec6d
jean.chung <jeannchung@gmail.com>
2018-12-20 14:25:09 -0800

    Updates

---
+++ precache-manifest.524ebc4d52d92258c21822c3fdc2d6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "67313100e7cfbae8cb82",
+    "url": "/clicky-game/static/js/main.67313100.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "67313100e7cfbae8cb82",
+    "url": "/clicky-game/static/css/main.adcd0151.chunk.css"
+  },
+  {
+    "revision": "9da532f7c3a1c8406cab5906057df9e2",
+    "url": "/clicky-game/index.html"
+  }
+];
